0abae98b89597813ed50f4a44392ceabeb7f7c6e
Saajan Patel <saajan.patel@sev1tech.com>
2019-07-24 13:08:38 -0400

    Working app

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }}, this is an about page</h1>
+    <p>Lorem ipsum</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
+
